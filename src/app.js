42b553e8542ac8e85fc4110808f07ec11dc037ec
yandapanda19 <i.am.kitiket@gmail.com>
2022-12-12 15:55:51 +0200

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 0) {
+    return `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 0) {
+    return `0${minutes}`;
+  }
+  let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  let day = days[date.getDay()];
+  return `Last updated: ${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
+  console.log(response.data.time);
   let temperatureElement = document.querySelector('#temperature');
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -13,6 +31,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity} %`;
   windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   // imgElement.innerHTML('src', response.data.condition.description.icon_url) = 
   console.log(response.data);
 }
