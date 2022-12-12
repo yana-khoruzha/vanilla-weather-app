9b279a3648fa64205aad29dbb1a8b3745d388402
yandapanda19 <i.am.kitiket@gmail.com>
2022-12-12 17:50:58 +0200

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -24,7 +24,6 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -33,14 +32,44 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute('src', response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.icon);
-  console.log(response.data);
+  celciusTemperature = Math.round(response.data.temperature.current);
 }
 
-let query = "Kiev";
-let key = '2aa5d6c3bt94of74014f9bf308abbb25';
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=metric`;
-
-  
+function search(query) {
+  let key = "2aa5d6c3bt94of74014f9bf308abbb25";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector('#cityInput');
+  search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove('active');
+  fahrenheitLink.classList.add('active');
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = celciusTemperature;
+}
+
+let celciusTemperature = null;
+let form = document.querySelector('#search-form');
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector('#fahrenheit-link');
+fahrenheitLink.addEventListener('click', displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
